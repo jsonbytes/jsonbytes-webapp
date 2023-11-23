862e7da9d83f01ec5eee1c3eb6c7f5afea8f686a
jsonbytes <jason@jsonbytes.com>
2023-11-23 09:29:10 -0600

    chore(): update medium link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { FaGithub, FaMedium, FaSquareXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   const socialLinks = [
-    { icon: <FaMedium />, link: "https://jsonbytes.medium.com" },
+    { icon: <FaMedium />, link: "https://blog.jsonbytes.com/" },
     { icon: <FaSquareXTwitter />, link: "https://twitter.com/jsonbytes" },
     { icon: <FaGithub />, link: "https://github.com/jsonbytes" },
   ];
